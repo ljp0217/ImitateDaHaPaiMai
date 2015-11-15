b0ddc0ff2347d6590b478d2a57101b7e0ce224b7
lijiangping <lijiangping@qq.com>
2015-11-15 18:42:45 +0800

    项目框架搭建

---
+++ imitateDaha/imitateDaha/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  imitateDaha
+//
+//  Created by ping on 15/11/15.
+//  Copyright (c) 2015年 LJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ imitateDaha/imitateDaha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  imitateDaha
+//
+//  Created by ping on 15/11/15.
+//  Copyright (c) 2015年 LJP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
